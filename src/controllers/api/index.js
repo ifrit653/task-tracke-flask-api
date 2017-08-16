515d08c556f61d2babdbf4a3ba0b3fe3a3cf3132
Ates Goral <ates.g@myplanet.com>
2017-08-16 13:42:09 -0400

    Make square controller HTTP-agnostic

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,9 +1,7 @@
-function handleTest(req, res, next) {
-  const number = parseInt(req.params.number, 10);
-
-  res.json({ result: number * number });
+function square(number) {
+  return number * number;
 }
 
 module.exports = Object.freeze({
-  handleTest
+  square
 });
--- src/routers/api/index.js
+++ src/routers/api/index.js
@@ -4,6 +4,12 @@ const controller = require('../../controllers/api');
 
 const router = express.Router();
 
-router.get('/square/:number', controller.handleTest);
+router.get('/square/:number', (req, res, next) => {
+  const number = parseInt(req.params.number, 10);
+
+  const result = controller.square(number);
+
+  res.json({ result });
+});
 
 module.exports = router;
