aadeab249808c63fe74a7f43bb93552e6e01b2df
Ates Goral <ates.g@myplanet.com>
2017-08-16 14:42:06 -0400

    Bring in body-parser, todo creation

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,12 +1,19 @@
-const todos = [{
-  id: 1,
-  text: 'Buy milk'
-}];
+const todos = [];
+
+let id = 0;
+
+function createTodo(text) {
+  todos.push({
+    id: ++id,
+    text
+  });
+}
 
 function readTodos() {
   return todos;
 }
 
 module.exports = Object.freeze({
+  createTodo,
   readTodos
 });
--- src/routers/api/index.js
+++ src/routers/api/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const controller = require('../../controllers/api');
 
 const router = express.Router();
+
+router.post('/todos', bodyParser.json({}), (req, res, next) => {
+  const text = req.body.text;
+
+  controller.createTodo({ text });
+
+  res.status(204).end();
+});
 
 router.get('/todos', (req, res, next) => {
   res.json(controller.readTodos());
